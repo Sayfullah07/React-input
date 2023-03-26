4f5a1692397a2280cb0723083f082be76969a88a
Sayfullah07 <magomadovu95@gmail.com>
2023-03-26 12:01:27 +0300

    first commit

---
+++ src/components/Checkbox.jsx
@@ -0,0 +1,17 @@
+function Checkbox(props) {
+    return (
+      <div>
+        <label htmlFor='check'></label>
+        <input
+          name='check'
+          type='checkbox'
+          checked={props.check}
+          onChange={props.handlerChanges}
+          className='check'
+        />
+      </div>
+    );
+  }
+  
+  export default Checkbox;
+  
---
+++ src/components/ToDo.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import Checkbox from "./Checkbox";
+
+function ToDO() {
+  const [todos, setTodos] = useState([
+    {
+      text: "HTML",
+    },
+    {
+      text: "CSS",
+    },
+    {
+      text: "JS",
+    },
+  ]);
+
+  const [text, setText] = useState("");
+  function handlerChange(e) {
+    setText(e.target.value);
+  }
+  const [check, setCheck] = useState(false);
+  function handlerChanges() {
+    setCheck(!check);
+  }
+
+  function handlerbtn() {
+    setTodos([
+      {
+        text: text,
+        done: check,
+      },
+      ...todos,
+    ]);
+    setText("");
+    setCheck(false);
+  }
+
+  return (
+    <div className='Todo'>
+      <div className='input'>
+        <input
+          type='text'
+          placeholder='Введите текст'
+          value={text}
+          onChange={handlerChange}
+        />
+        <button onClick={handlerbtn} className='btn'>
+          add
+        </button>
+        <Checkbox check={check} handlerChanges={handlerChanges} />
+      </div>
+
+      {todos.map((item, idx) => {
+        return (
+          <div className='text'>
+            <li key={idx} className='text-li'>
+              {item.text}
+            </li>
+            <input type='checkbox' checked={item.done} />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default ToDO;
